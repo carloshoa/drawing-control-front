2c6a74d7c2543b7d8343fc358866be5e39696cc9
Carlos Arantes <carloshoarantes@gmail.com>
2022-08-15 23:02:10 -0300

    add modal and form adjusts

--- src/components/pages/ProjectDetails/ProjectDetails.jsx
+++ src/components/pages/ProjectDetails/ProjectDetails.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 import {
-  Form, Col, Button, Accordion, Card,
+  Form, Col, Button, Accordion, Card, Modal,
 } from 'react-bootstrap';
 
 import { useAccordionButton } from 'react-bootstrap/AccordionButton';
@@ -21,7 +21,7 @@ import { createProjectDrawing, getOneProject } from '../../../services/api';
 import './ProjectDetails.css';
 
 const ProjectDetails = () => {
-  // const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
   // const [searchName, setSearchName] = useState('');
   const [project, setProject] = useState({});
   const [drawing, setDrawing] = useState({});
@@ -39,22 +39,8 @@ const ProjectDetails = () => {
     }
   };
 
-  // const handleChange = (e) => {
-  //   setSearchName(e.target.value);
-  // };
-
-  // eslint-disable-next-line react/no-unstable-nested-components
-  const CustomToggle = ({ children, eventKey }) => {
-    const decoratedOnClick = useAccordionButton(eventKey, () => (console.log('totally custom!')));
-    return (
-      <button
-        type="button"
-        onClick={decoratedOnClick}
-      >
-        {children}
-      </button>
-    );
-  };
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
 
   const DrawingSchema = yup.object().shape({
     drawingNumber: yup.string().required('Required Field').max(50, 'Maximum of 50 characters'),
@@ -88,6 +74,7 @@ const ProjectDetails = () => {
 
   useEffect(() => {
     getProjectById();
+    handleClose();
   }, [drawing]);
 
   return (
@@ -104,73 +91,95 @@ const ProjectDetails = () => {
         <h1>
           Project Detail
         </h1>
+
+        <Button variant="primary" onClick={handleShow}>
+          New Drawing
+        </Button>
         <p>{project.title}</p>
         <div className="project-card ">
           <h2>Drawing</h2>
           <ul>
-            {project.drawings && project.drawings.map((drawing) => (
-                <li>{drawing.drawingNumber}</li>
-                <li>{drawing.title}</li>
-                <hr></hr>
-              </Link>
+            {project.drawings && project.drawings.map((eachDrawing) => (
+              <div>
+
+                <Link to="/">
+                  <li>
+                    <h4>
+                      {eachDrawing.drawingNumber}
+                    </h4>
+                    <p>
+                      {eachDrawing.title}
+                    </p>
+                    {console.log(eachDrawing)}
+                  </li>
+                </Link>
+              </div>
             ))}
           </ul>
 
         </div>
-        <Accordion>
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>CRETE NEW DRAWING</Accordion.Header>
-            <Accordion.Body>
-              <Form onSubmit={handleSubmit}>
-                <Form.Group as={Col} md="12" controlId="validationCustom01">
-                  <Form.Label>Drawing Number</Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="drawingNumber"
-                    value={values.drawingNumber}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    isValid={touched.drawingNumber && !errors.drawingNumber}
-                    isInvalid={touched.drawingNumber && errors.drawingNumber}
-                  />
-                  <Form.Control.Feedback>Ok!</Form.Control.Feedback>
-                  <Form.Control.Feedback type="invalid">{errors.drawingNumber}</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="12" controlId="validationCustom01">
-                  <Form.Label>Title</Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="title"
-                    value={values.title}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    isValid={touched.title && !errors.title}
-                    isInvalid={touched.title && errors.title}
-                  />
-                  <Form.Control.Feedback>Ok!</Form.Control.Feedback>
-                  <Form.Control.Feedback type="invalid">{errors.title}</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="12" controlId="validationCustom01">
-                  <Form.Label>Pages</Form.Label>
-                  <Form.Control
-                    type="number"
-                    name="pages"
-                    value={values.number}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    isValid={touched.number && !errors.number}
-                    isInvalid={touched.number && errors.number}
-                  />
-                  <Form.Control.Feedback>Ok!</Form.Control.Feedback>
-                  <Form.Control.Feedback type="invalid">{errors.title}</Form.Control.Feedback>
-                </Form.Group>
-                <Button type="submit" className="login-submit-button mt-3"> Create New Project </Button>
-              </Form>
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
-
       </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group as={Col} md="12" controlId="validationCustom01">
+              <Form.Label>Drawing Number</Form.Label>
+              <Form.Control
+                type="text"
+                name="drawingNumber"
+                value={values.drawingNumber}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isValid={touched.drawingNumber && !errors.drawingNumber}
+                isInvalid={touched.drawingNumber && errors.drawingNumber}
+              />
+              <Form.Control.Feedback>Ok!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">{errors.drawingNumber}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="12" controlId="validationCustom01">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                name="title"
+                value={values.title}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isValid={touched.title && !errors.title}
+                isInvalid={touched.title && errors.title}
+              />
+              <Form.Control.Feedback>Ok!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">{errors.title}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="12" controlId="validationCustom01">
+              <Form.Label>Pages</Form.Label>
+              <Form.Control
+                type="number"
+                name="pages"
+                value={values.number}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isValid={touched.number && !errors.number}
+                isInvalid={touched.number && errors.number}
+              />
+              <Form.Control.Feedback>Ok!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">{errors.title}</Form.Control.Feedback>
+            </Form.Group>
+            {/* <Button type="submit" className="login-submit-button mt-3"> Create New Project </Button> */}
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSubmit}>
+            Create New Project
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </TemplatePrivate>
   );
 };
